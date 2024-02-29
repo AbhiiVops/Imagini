582930bced09206ad1fb32777dffa26642fa80e0
AbhiiVops <abhiias2003@gmail.com>
2024-03-01 00:18:19 +0530

    Implemented the API usage Limit feature: - Set the limit to 5 for each user - Added this features for all the available options - Used Postgres DB for this - Used Supabase DB for implementing this - Used Prisma ORM In supabase

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi } from "openai";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 
@@ -37,11 +39,19 @@ export async function POST(
             return new NextResponse("Resolution is required", { status: 400 });
         }
 
+        const freeTrial = await checkApiLimit();
+
+        if (!freeTrial) {
+            return new NextResponse("Free trial limit reached", { status: 403 });
+        }
+
         const response = await openai.createImage({
            prompt,
            n: parseInt(amount, 10),
            size: resolution,
         });
+
+        await increaseApiLimit();
 
         return NextResponse.json(response.data.data);
     }
